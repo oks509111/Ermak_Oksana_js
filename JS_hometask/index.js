3d4a799e1823fefcb88bec3ed477035263574e5c
oksana <oksanka9191@gmail.com>
2021-07-15 00:26:30 +0300

    homework2

--- JS_hometask/index.js
+++ JS_hometask/index.js
@@ -1,149 +1,90 @@
 "use strict";
- //1
-function hello1 (){
-  let text = "Привет, JavaScript!";
-  return text;
-}
-console.log( hello1() );
+//1
+let array1 = [1, 2, 3, 4, 4, 5, 2, 9, 4];
+createUniqueArray(array1);
 
 //2
-function hello2 (name) {
-
-  if (!name) {
-    console.log('Привет, гость!');
-  } else {
-    console.log('Привет, ' + name);
-  }
-  //name ? console.log ('Привет, ' + name) : console.log('Привет, гость!');
+let makeAdultUsers = users => {
+    let usersAfterYear = users.map(u => { u.age = u.age + 1; return u;}).filter(u => u.age >= 18);
+    console.log(usersAfterYear);
+    return usersAfterYear;
 }
-hello2("Вася");
+let users1 = [
+    {id: 1, name: "Вася", age: 19},
+    {id: 2, name: "Петя", age: 18},
+    {id: 3, name: "Маша", age: 17},
+    {id: 4, name: "Света", age: 18},
+    {id: 5, name: "Наташа", age: 17},
+    {id: 6, name: "Женя", age: 12},
+    {id: 7, name: "Коля", age: 30},
+    {id: 8, name: "Настя", age: 16},
+    {id: 9, name: "Антон", age: 19},
+    {id: 10, name: "Иван", age: 20},
+    {id: 11, name: "Павел", age: 16},
+    {id: 12, name: "Денис", age: 20}
+];
+let usersAfterYear = makeAdultUsers(users1);
 
 //3
-function mul(n, m){
-  return m * n;
+let createUniqueObjectArray = array => {
+    let obj = {};
+
+    for (let i = 0, len = array.length; i < len; i++) {
+        obj[array[i]['age']] = array[i];
+    }
+
+    array = [];
+    for (let key in obj) {
+        array.push(obj[key]);
+    }
+    console.log(array);
+    return array;
 }
-console.log (mul(2,4));
+
+let usersAfterYearUniqueAge = createUniqueObjectArray(usersAfterYear);
 
 //4
-function repeat(string, n) {
-  n = !n ? 2 : n;  
-  string = !string ? "" : string;  
-  let text = ""; 
-  for (let i = 0; i < n; i++) { 
-    text = text + string;
-  }
-  return text;
+let fibonachi = n => {
+    return n <= 1 ? n : fibonachi(n - 1) + fibonachi(n - 2);
 }
-console.log (repeat("привет", 4));
-
+console.log(fibonachi(8));
 
 //5
-function rgb(a,b,c) {
-
-a = !a ? 0 : a;
-b = !b ? 0 : b;
-c = !c ? 0 : c;
-
-return "rgb(" + a + ", " + b + ", " + c + ")";
+let factorial = n => {
+    return (n != 1) ? n * factorial(n - 1) : 1;
 }
-console.log (rgb(23,100,134));
+console.log(factorial(10));
 
 //6
-function avg() {
-
- let sum = 0;
-    for (i=0; i < arguments.length; i++) {
-        sum += arguments[i];
-    }
-  console.log (sum / arguments.length);
+let func = () => {
+    let currentCount = 1;
+    return function() {
+        return currentCount++;
+    };
 }
-let numbers = [3, 2, 4];
-avg.apply(this, numbers);
-
-
+let counter = func();
+console.log(counter());
+console.log(counter());
+console.log(counter());
 
 //7
-function words(n) {
-  n = !n ? 0 : n;
-  let num = n % 10;
-  console.log (num);
-  let result = "";
-  if (num > 1 && num < 5) {
-    result = n + " " + "товара";
-  } else if (n > 10 || n == 0) {
-    result = n + " " + "товаров";
-  }
-
-   if (num == 1) {
-    result = n + " " + "товар";
-  } 
-
-  console.log (result);
+function User(first_name, second_name, age, city) {
+    this.first_name = first_name;
+    this.second_name = second_name;
+    this.age = age;
+    this.city = city;
 }
-words(25);
+
+let user1 = new User("Peter", "First", 33, "Saint-Petersburg");
+let user2 = new User("Иван", "Иванов", 45, "Moscow");
+console.log(user1);
+console.log(user2);
 
 //8
+function fullInfo() {
+    console.log(this);
+}
 
-unction ur2(a, b, c) { 
-  let d = b*b - 4*a*c; 
-  let x1 = 0; 
-  let x2 = 0; 
-  if (d > 0) { 
-      x1 = (-b + Math.sqrt(d)) / 2*a; 
-      x2 = (-b - Math.sqrt(d)) / 2*a; 
-      console.log("x1 =" + x1 + "; x2 = " + x2); 
-  } else if (d === 0) { 
-      x1 = -b / (2*a); 
-      console.log("x =" + x1); 
-  } else { 
-      console.log("нет решений"); 
-  } 
-} 
-//ax2 + bx + c = 0; 
-ur2(1,4,1); 
-let f = x => x === undefined ? 0 : 1; 
-console.log(f()); 
-
-//10
-let sum = object => { 
-  let result = 0; 
-  for (const property in object) { 
-      result += object[property]; 
-  } 
-  return result; 
-} 
-console.log(sum({a: 4, b: 9})); 
-
-
-//11
-let sortAscending = array => { 
-  array.sort((a, b) => a - b); 
-  return array.indexOf(6); 
-} 
-
-const array = [1, 2, 56, 28, 90, 5, 6]; 
-console.log(sortAscending(array)); 
-
-//12 пока я максимально запуталась нужно время на подумать
-let splitUsers = array => { 
-  let minors = array.filter(a => a.age < 18).sort((a,b) => b.age - a.age); 
-  let adults = array.filter(a => a.age >= 18).sort((a,b) => b.age - a.age); 
-  console.log(minors); 
-  console.log(adults); 
-} 
-
-let removeMinors = array => { 
-
-  for (const object in array) { 
-      console.log("df"); 
-      console.log(property); 
-      if (property < 18) { 
-          array.splice(array.indexOf(property), 1); 
-      } 
-  } 
-} 
-
-
-let items = [{name: 'Ivan', age: 18}, {name: 'Petr', age: 12}, {name: 'Sidor', age: 25}, {name: 'Pavel', age: 24}, {name: 'Sasha', age: 29}] 
-splitUsers(items); 
-//removeMinors(items);
+user1.print = fullInfo;
+user2.print = fullInfo;
+user1.print();
