9f57b7e5a8f7c6ad0f2dc3aa3018acee2ffbe1f6
oksana <oksanka9191@gmail.com>
2021-07-22 15:26:31 +0300

    4_tasks

--- JS_hometask/index.js
+++ JS_hometask/index.js
@@ -1,112 +1,18 @@
 "use strict";
+
 //1
-let array1 = [1, 2, 3, 4, 4, 5, 2, 9, 4];
+function cleanRoom() {
+    setTimeout(() => console.log("Команата убрана"), 5000);
+}
 
+let cleanRoom = new Promise (function(resolve, reject){
+    setTimeout(() => resolve (console.log("Команата убрана")), 1000);
+});
 
-let arrUnique = array1.reduce((num1, num2) => {
-    if (num1.indexOf(num2) === -1) {
-      num1.push(num2);
-    }
-    return num1;
-  }, []);
-  
-  console.log(arrUnique);
-
-
-/////
-let array1 = [1, 2, 3, 4, 4, 5, 2, 9, 4];
-let arrUnique = [...new Set([1, 2, 3, 4, 4, 5, 2, 9, 4])];
-console.log(arrUnique);
-
-// 
-
-let array1 = [1, 2, 3, 4, 4, 5, 2, 9, 4];
-let arrUnique =array1.filter((item, index) => array1.indexOf(item)===index);
-console.log(arrUnique);
-//
-let array1 = [1, 2, 3, 4, 4, 5, 2, 9, 4];
-let arrUnique = array1.reduce((unique, item)=>
-unique.includes(item) ? unique : [... unique, item], []);
-console.log(arrUnique);
-
-
+cleanRoom.then (
+    result => console.log (result),
+    error => console.log (error)
+);
 
 //2
-let makeAdultUsers = users => {
-    let usersAfterYear = users.map(u => { u.age = u.age + 1; return u;}).filter(u => u.age >= 18);
-    console.log(usersAfterYear);
-    return usersAfterYear;
-}
-let users1 = [
-    {id: 1, name: "Вася", age: 19},
-    {id: 2, name: "Петя", age: 18},
-    {id: 3, name: "Маша", age: 17},
-    {id: 4, name: "Света", age: 18},
-    {id: 5, name: "Наташа", age: 17},
-    {id: 6, name: "Женя", age: 12},
-    {id: 7, name: "Коля", age: 30},
-    {id: 8, name: "Настя", age: 16},
-    {id: 9, name: "Антон", age: 19},
-    {id: 10, name: "Иван", age: 20},
-    {id: 11, name: "Павел", age: 16},
-    {id: 12, name: "Денис", age: 20}
-];
-let usersAfterYear = makeAdultUsers(users1);
 
-//3
-
-
-let usersAfterYearUniqueAge =  usersAfterYear.reduce((users1, users) => {
-    if (users1.indexOf(users) === -1){
-        users1.push(users);
-    }
-    return users1;
-}, []);
-
-console.log(usersAfterYear);
-
-
-//4
-let fibonachi = n => {
-    return n <= 1 ? n : fibonachi(n - 1) + fibonachi(n - 2);
-}
-console.log(fibonachi(8));
-
-//5
-let factorial = n => {
-    return (n != 1) ? n * factorial(n - 1) : 1;
-}
-console.log(factorial(10));
-
-//6
-let func = () => {
-    let currentCount = 1;
-    return function() {
-        return currentCount++;
-    };
-}
-let counter = func();
-console.log(counter());
-console.log(counter());
-console.log(counter());
-
-//7
-function User(first_name, second_name, age, city) {
-    this.first_name = first_name;
-    this.second_name = second_name;
-    this.age = age;
-    this.city = city;
-}
-
-let user1 = new User("Peter", "First", 33, "Saint-Petersburg");
-let user2 = new User("Иван", "Иванов", 45, "Moscow");
-console.log(user1);
-console.log(user2);
-
-//8
-User.prototype.info = function () {
-    console.log(this.first_name, this.second_name, this.age, this.city);
-};
-
-user1.info();
-user2.info();
