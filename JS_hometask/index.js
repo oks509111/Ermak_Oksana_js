a84b5d651f2665929d6fa99a8c30af4574c0415c
oksana <oksanka9191@gmail.com>
2021-07-22 17:44:41 +0300

    4_tasks

--- JS_hometask/index.js
+++ JS_hometask/index.js
@@ -14,5 +14,27 @@ cleanRoom.then (
     error => console.log (error)
 );
 
-//2
+//2,3
 
+ let cleanRoom = (dirtyLevel = 0) => { 
+ 
+    return new Promise((resolve, reject) => { 
+ 
+        let time = dirtyLevel * 1000; 
+        setTimeout(() => { 
+            if (dirtyLevel < 0 || dirtyLevel > 10) { 
+                reject(`Значение параметра может быть от 0 до 10`); 
+            } else { 
+                resolve(time / 1000); 
+            } 
+        }, time); 
+    }); 
+} 
+ 
+cleanRoom(3) 
+    .then(result => { 
+        console.log(`Уборка проведена успешно за ${result} секунд`); 
+    }) 
+    .catch(err => { 
+        console.log(err); 
+    });
