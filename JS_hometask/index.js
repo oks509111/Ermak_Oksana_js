dc900b7ac07710ba2c6136f46d50296af453113e
oksana <oksanka9191@gmail.com>
2021-07-28 15:40:07 +0300

    fix_first_task

--- JS_hometask/index.js
+++ JS_hometask/index.js
@@ -5,14 +5,19 @@ function cleanRoom() {
     setTimeout(() => console.log("Команата убрана"), 5000);
 }
 
-let cleanRoom = new Promise (function(resolve, reject){
-    setTimeout(() => resolve (console.log("Команата убрана")), 1000);
-});
+let cleanRoom = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve (console.log("Команата убрана"));
+    }, 5000);
+  });
 
 cleanRoom.then (
     result => console.log (result),
     error => console.log (error)
 );
+
+
+
 
 //2,3
 
