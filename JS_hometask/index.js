a004ce0f0b0c3ae425828c0522e56b346e3cc678
oksana <oksanka9191@gmail.com>
2021-08-02 15:05:03 +0300

    fix

--- JS_hometask/index.js
+++ JS_hometask/index.js
@@ -22,20 +22,19 @@ cleanRoom().then (
 
 //2,3
 
- let cleanRoom = (dirtyLevel = 0) => { 
+const cleanRoom = (dirtyLevel  = 0) => { 
  
     return new Promise((resolve, reject) => { 
  
-        let time = dirtyLevel * 1000; 
         setTimeout(() => { 
             if (dirtyLevel < 0 || dirtyLevel > 10) { 
                 reject(`Значение параметра может быть от 0 до 10`); 
             } else { 
-                resolve(time / 1000); 
+                resolve(dirtyLevel); 
             } 
-        }, time); 
-    }); 
-} 
+        }, dirtyLevel * 1000); 
+      }); 
+}
  
 cleanRoom(3) 
     .then(result => { 
